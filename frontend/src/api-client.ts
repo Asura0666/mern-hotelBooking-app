99e397508f492f7682623da56710878e3bad098b
Asura0666 <dhirajlande18@gmail.com>
2024-01-26 12:01:38 +0530

    fixed fetch issue

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -4,7 +4,7 @@ import { SignInFormData } from "./pages/SignIn";
 import { HotelType } from "../../backend/src/shared/types";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:7000";
+  import.meta.env.VITE_API_BASE_URL || "";
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
